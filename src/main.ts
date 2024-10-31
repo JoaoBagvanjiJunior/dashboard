6ba91b12e5a4f7a1f6d33d32251510db53a3c7d1
JoaoBagvanji <jbagvanji54@gmail.com>
2024-10-31 15:38:13 -0300

    'Autorizacoes no main.ts para rota 3000'

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000', // URL do seu frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // se você precisar enviar cookies ou autenticação
+  });
 
   app.useGlobalPipes(new ValidationPipe({
     whitelist:true,
