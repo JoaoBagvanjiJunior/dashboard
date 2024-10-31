af8dbe01dd15d9c0e3bbc8033c277c80d5717f63
JoaoBagvanji <jbagvanji54@gmail.com>
2024-10-31 00:56:38 -0300

    App Backend Dashboard Admin Codako

---
+++ src/auth/dto/auth.dto.ts
@@ -0,0 +1,13 @@
+import { IsEmail, IsString } from "class-validator";
+
+
+export class LoginDto{
+    @IsString()
+    
+
+    @IsEmail()
+    username: string;
+
+    @IsString()
+    password: string;
+}
